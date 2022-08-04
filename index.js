16124f53798f6b8b6e24d522da811d7271389d1b
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-08-04 21:39:00 +0100

    extenstion 1

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 const app = express();
+const qs = require("qs");
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+app.set("query parser", function (str) {
+  return qs.parse(str, {});
+});
 
 const myCounter = { counter: 0 };
 
@@ -34,6 +39,14 @@ app.post("/counter/decrement", (req, res) => {
 
 app.post("/counter/double", (req, res) => {
   myCounter.counter *= 2;
+  res.status(201).json(myCounter);
+});
+
+app.put("/counter", (req, res) => {
+  if (req.query.value) {
+    myCounter.counter = req.query.value;
+  }
+  console.log(myCounter.counter);
   res.status(201).json(myCounter);
 });
 
